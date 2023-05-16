e79758310966628240bfcbaf6715c59d5549aee9
이승희 <127372877+spring41@users.noreply.github.com>
2023-05-16 09:13:03 +0900

    Docs

--- src/main/java/kb03/multicampus/petandmet/controller/CardController.java
+++ src/main/java/kb03/multicampus/petandmet/controller/CardController.java
@@ -6,14 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CardController {
-
-	@Autowired
-	private CardRepository cardRepository;
-	
-	
-	@GetMapping({"","/"})
-	public String index() {
-		return "index";
-	}
+	 @Autowired
+	 private CardRepository cardRepository;
+	 
+	 
 	
 }
