cf5e34467905eeab3345cfdd554ade8d96862599
ykchoi7 <7dbsrud7@gmail.com>
2023-05-17 15:17:35 +0900

    insurance.jsp
    update

--- src/main/java/kb03/multicampus/petandmet/controller/InsuranceController.java
+++ src/main/java/kb03/multicampus/petandmet/controller/InsuranceController.java
@@ -2,14 +2,20 @@ package kb03.multicampus.petandmet.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import kb03.multicampus.petandmet.dto.BreedInsuranceDto;
 import kb03.multicampus.petandmet.dto.InsuranceDto;
 import kb03.multicampus.petandmet.dto.PetDto;
+import kb03.multicampus.petandmet.dto.UserDto;
 import kb03.multicampus.petandmet.mapper.InsuranceMapper;
+import kb03.multicampus.petandmet.service.InsuranceService;
 
 @Controller
 public class InsuranceController {
@@ -17,12 +23,51 @@ public class InsuranceController {
 	@Autowired
 	InsuranceMapper mapper;
 	
+	@Autowired
+	InsuranceService service;
+	
 	@GetMapping("/insurances")
 	public String insurance(Model model) {
 		//db에서 보험 리스트 가져오기		
         List<InsuranceDto> ins = mapper.findAll();
+
+        for (InsuranceDto i : ins) {
+    		String msg ="";
+
+			int max = (int)(i.getMax_age()/12);
+			i.setMax_age(max);
+			
+			int rate = (i.getCoverage_ratio()*100);
+			i.setCoverage_ratio(rate);
+			
+			int year = (int)(i.getPeriod()/12);
+			i.setPeriod(year);
+			System.out.println(i.getPatella());
+			
+			if (i.getPatella()==true) {
+				msg = "보장";
+				
+			} else {
+				msg = "미포함";
+			}
+			System.out.println(msg);
+			model.addAttribute("p", msg); 
+
+		}
 		model.addAttribute("ins", ins);
 		
 		return "insurances";
 	}
+	
+	//petdto에서 pet breed를 가져오기 
+//	@GetMapping
+//    public String insurance(Model model, HttpServletRequest req) {
+//        HttpSession session = req.getSession();
+//        Object breed = session.getAttribute("breed"); //품종 찾아옴
+//        BreedInsuranceDto bi = (BreedInsuranceDto) breed;
+//        int ino = bi.getIno();//품종에 맞는 보험 가져오기
+//        List<InsuranceDto> ins = InsuranceMapper.findByBreed(breed);
+//        model.addAttribute("insurances", ins);
+//        return "insurances";
+//    }
 }
--- src/main/webapp/WEB-INF/jsp/insurances.jsp
+++ src/main/webapp/WEB-INF/jsp/insurances.jsp
@@ -16,12 +16,23 @@
 			
 			<h2>${i.name}</h2>
 			<p>${i.company}</p>
-			<p>혜택: </p> ${i.benefits} <br>
+			<p>대상 연령: ${i.min_age}개월 ~ ${i.max_age}세</p>
+			<p>보장 비율: ${i.coverage_ratio}%</p>
+			<p>기간: ${i.period}년</p>
+			<p>혜택: ${i.benefits} </p>
+			<p>1년 최대 보장 금액: ${i.annual_limit}</p>
+			<p>1일 최대 보장 금액: ${i.daily_limit}</p>
+			
+			<p>슬개골 보장 여부: ${p}</p>
+			<p>구강질환 보장 여부: ${i.tooth}</p>
+			<p>피부질환 보장 여부: ${i.skin}</p>
+			<p>스케일링 여부: ${i.scaling} </p>
 			
 			<button type="button" onclick="location.href='insurance'">상세보기</button>
 			</c:forEach>
+			<br>
+			
 		</div>
 	</main>
-	<button type="button" onclick="location.href='insurance'">상세보기</button>
 </body>
 </html>
