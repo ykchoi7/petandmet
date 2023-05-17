1f1f41a9165582dfaf5b10a00aa8284c069773c6
Park HyungKyu <gudrb4869@naver.com>
2023-05-17 15:01:29 +0900

    Update ProductController.java

--- src/main/java/kb03/multicampus/petandmet/controller/ProductController.java
+++ src/main/java/kb03/multicampus/petandmet/controller/ProductController.java
@@ -1,16 +1,21 @@
 package kb03.multicampus.petandmet.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import kb03.multicampus.petandmet.dto.PetDto;
 import kb03.multicampus.petandmet.dto.ProductDto;
+import kb03.multicampus.petandmet.dto.UserDto;
+import kb03.multicampus.petandmet.service.PetService;
 import kb03.multicampus.petandmet.service.ProductService;
 import lombok.RequiredArgsConstructor;
 
@@ -20,9 +25,18 @@ import lombok.RequiredArgsConstructor;
 public class ProductController {
 
 	private final ProductService productService;
+	private final PetService petService;
 	
     @GetMapping
-    public String products() {
+    public String products(HttpSession session, Model model) {
+    	// 2023-05-16 테스트 하는동안만 주석 처리 했음
+//    	Object object = session.getAttribute("user");
+//    	if (object == null) {
+//    		return "redirect:/login";
+//    	}
+//    	UserDto user = (UserDto) object;
+    	PetDto pet = petService.findByNo(1);
+    	model.addAttribute("pet", pet);
         return "products";
     }
     
