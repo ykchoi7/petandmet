fcf72f0ccb14275e3fea23b4981142b98e84ba03
ykchoi7 <7dbsrud7@gmail.com>
2023-05-17 10:17:44 +0900

    insurance
    ver2

--- src/main/java/kb03/multicampus/petandmet/controller/InsuranceController.java
+++ src/main/java/kb03/multicampus/petandmet/controller/InsuranceController.java
@@ -2,19 +2,26 @@ package kb03.multicampus.petandmet.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import kb03.multicampus.petandmet.dto.InsuranceDto;
+import kb03.multicampus.petandmet.dto.PetDto;
+import kb03.multicampus.petandmet.mapper.InsuranceMapper;
 
 @Controller
 public class InsuranceController {
 	
+	@Autowired
+	InsuranceMapper mapper;
+	
 	@GetMapping("/insurances")
 	public String insurance(Model model) {
 		//db에서 보험 리스트 가져오기		
-		model.addAttribute("보험 상세 정보");
+        List<InsuranceDto> ins = mapper.findAll();
+		model.addAttribute("ins", ins);
 		
 		return "insurances";
 	}
