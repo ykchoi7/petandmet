336deca524a2e459cc8792eb9d3e91d83df4a137
이승희 <127372877+spring41@users.noreply.github.com>
2023-05-18 10:48:44 +0900

    Docs

--- src/main/java/kb03/multicampus/petandmet/service/CardService.java
+++ src/main/java/kb03/multicampus/petandmet/service/CardService.java
@@ -1,5 +1,19 @@
 package kb03.multicampus.petandmet.service;
 
-public class CardService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kb03.multicampus.petandmet.dto.CardDto;
+import kb03.multicampus.petandmet.mapper.CardMapper;
+
+@Service
+public class CardService {
+	@Autowired
+    CardMapper cardmapper;
+
+    public List<CardDto> findAll(){
+        return cardmapper.findAll();
+    }
 }
